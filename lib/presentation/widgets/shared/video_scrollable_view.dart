b353271683431e1dd2a19d54c969700515dfd200
CevicheMixto23 <alfarodezapiero@gmail.com>
2024-10-17 18:55:52 -0500

    feat: :art: Finish the app with the static videos
    The app is finished with static videos, todo: dynamic videos

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik_devtalles/domain/entities/video_post.dart';
+import 'package:toktik_devtalles/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik_devtalles/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,15 +10,27 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.yellow),
-        Container(color: Colors.lightGreen)
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            //Video Player + gradiente
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
+            //Botones
+            Positioned(
+                bottom: 40, right: 20, child: VideoButtons(video: videoPost))
+          ],
+        );
+      },
     );
   }
 }
